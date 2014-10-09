eecd53e6f81e30d89d807a3bbd9564022eab8a87
Michael Aquilina <michaelaquilina@gmail.com>
2014-10-09 22:59:55 +0100

    Add Jinja2 for rendering data into an HTML table

--- tube.py
+++ tube.py
@@ -1,8 +1,10 @@
 #! /usr/bin/python
 
+from datetime import datetime
+import jinja2
+import yaml
 import pandas as pd
 import requests
-import yaml
 
 from envelopes import Envelope
 
@@ -11,28 +13,51 @@ mailing_list = [('michaelaquilina@gmail.com', 'Michael Aquilina')]
 # Line for which to retrieve the status
 lines = ['jubilee', 'bakerloo', 'metropolitan', 'circle', 'hammersmith-city']
 
-r = requests.get('http://api.tfl.gov.uk/line/{}/Status?'.format(','.join(lines)))
+detail = True
+
+# http://api.tfl.gov.uk/
+r = requests.get('http://api.tfl.gov.uk/line/{}/Status?detail={}'.format(','.join(lines), detail))
 
 if r.ok:
     data = pd.DataFrame(r.json(), columns=['id', 'name', 'lineStatuses'])
-    data['Status'] = ''
-    data['Category'] = ''
+    data['status'] = ''
+    data['statusSeverity'] = 0
+    data['disruption'] = ''
     data.set_index('id', inplace=True)
 
     for index, status in data['lineStatuses'].iteritems():
-        data.ix[index]['Status'] = status[0]['statusSeverityDescription']
+        data['status'].ix[index] = status[0]['statusSeverityDescription']
+        data['statusSeverity'].ix[index] = status[0]['statusSeverity']
 
         if 'disruption' in status[0]:
-            data.ix[index]['Category'] = status[0]['disruption']['category']
+            data.ix[index]['disruption'] = status[0]['disruption']['category']
 
     del data['lineStatuses']
     print data
+
+    t = jinja2.Template('''
+        <p>The Tube Status for {{ today.strftime('%A %d %B %Y') }}</p>
+        <table border="1">
+            <tr>
+                <th>Line</th>
+                <th>Status</th>
+                <th>Details</th>
+            </tr>
+            {% for index, row in data.iterrows() %}
+            <tr style="color: {{'green' if row.statusSeverity == 10 else 'red'}}">
+                <td>{{ row['name'] }}</td>
+                <td>{{ row['status'] }}</td>
+                <td>{{ row['disruption'] }}</td>
+            </tr>
+            {% endfor %}
+        </table>
+    ''')
 
     env = Envelope(
         to_addr=mailing_list,
         from_addr=('my.tube.notifier@gmail.com', 'Tube Notifier'),
         subject='Today\'s Tube Status',
-        html_body=data.to_html()
+        html_body=t.render(data=data, today=datetime.now())
     )
 
     config = yaml.load(open('config.yml'))
